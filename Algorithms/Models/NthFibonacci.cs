9aba8a2a64c417db6532245e61cf5f2102e14982
Adetayo Olaitan <olaitan.adetayo.com>
2020-05-24 14:28:03 +0100

    added solution 3

--- Algorithms/Models/NthFibonacci.cs
+++ Algorithms/Models/NthFibonacci.cs
@@ -42,5 +42,19 @@ namespace Algorithms.Models
 			}
 		}
 
+		public static int GetNthFibSolution3(int n)
+		{
+			int[] lastTwo = { 0, 1 };
+			int counter = 3;
+			while (counter <= n)
+			{
+				int nextFib = lastTwo[0] + lastTwo[1];
+				lastTwo[0] = lastTwo[1];
+				lastTwo[1] = nextFib;
+				counter++;
+			}
+			return n > 1 ? lastTwo[1] : lastTwo[0];
+		}
+
 	}
 }
