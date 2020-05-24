784bdc15fe102d97c8f17359f93e964e05f99a81
Adetayo Olaitan <olaitan.adetayo.com>
2020-05-24 14:17:45 +0100

    added solution 2

--- Algorithms/Models/NthFibonacci.cs
+++ Algorithms/Models/NthFibonacci.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Algorithms.Models
 {
     public class NthFibonacci
@@ -18,5 +20,27 @@ namespace Algorithms.Models
 				return GetNthFibSolution1(n - 1) + GetNthFibSolution1(n - 2);
 			}
 		}
+
+		public static int GetNthFibSolution2(int n)
+		{
+			Dictionary<int, int> memorize = new Dictionary<int, int>();
+			memorize.Add(1, 0);
+			memorize.Add(2, 1);
+			return GetNthFibSolution2(n, memorize);
+		}
+
+		public static int GetNthFibSolution2(int n, Dictionary<int, int> memorize)
+		{
+			if (memorize.ContainsKey(n))
+			{
+				return memorize[n];
+			}
+			else
+			{
+				memorize.Add(n, GetNthFibSolution2(n - 1, memorize) + GetNthFibSolution2(n - 2, memorize));
+				return memorize[n];
+			}
+		}
+
 	}
 }
