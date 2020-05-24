a31cbcb6ecd397776778def098f80291ba8a672e
Adetayo Olaitan <olaitan.adetayo.com>
2020-05-24 14:11:34 +0100

    added solution 1

---
+++ Algorithms/Models/NthFibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class NthFibonacci
+    {
+		public static int GetNthFibSolution1(int n)
+		{
+			if (n == 2)
+			{
+				return 1;
+			}
+			else if (n == 1)
+			{
+				return 0;
+			}
+			else
+			{
+				return GetNthFibSolution1(n - 1) + GetNthFibSolution1(n - 2);
+			}
+		}
+	}
+}
