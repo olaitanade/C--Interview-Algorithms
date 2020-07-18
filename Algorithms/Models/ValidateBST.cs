b54155025e04f2741ce000f46809dd5e2f3f279f
Adetayo Olaitan <olaitan.adetayo.com>
2020-07-18 21:01:29 +0100

    added the validate BST algorithm

---
+++ Algorithms/Models/ValidateBST.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class ValidateBST
+    {
+		public static bool ValidateBst(BST tree)
+		{
+			return ValidateBst(tree, Int32.MinValue, Int32.MaxValue);
+		}
+
+		public static bool ValidateBst(BST tree, int minValue, int maxValue)
+		{
+			if (tree.value < minValue || tree.value >= maxValue)
+			{
+				return false;
+			}
+			if (tree.left != null && !ValidateBst(tree.left, minValue, tree.value))
+			{
+				return false;
+			}
+			if (tree.right != null && !ValidateBst(tree.right, tree.value, maxValue))
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public class BST
+		{
+			public int value;
+			public BST left;
+			public BST right;
+
+			public BST(int value)
+			{
+				this.value = value;
+			}
+		}
+	}
+}
