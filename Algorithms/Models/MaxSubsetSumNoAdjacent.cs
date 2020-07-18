86b90ad30f2144e78a67f7bd6c0d42527c17f7e2
Adetayo Olaitan <olaitan.adetayo.com>
2020-07-18 22:30:56 +0100

    added Max Subset sum no adjacent solution

---
+++ Algorithms/Models/MaxSubsetSumNoAdjacent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class MaxSubsetSumNoAdjacent
+    {
+		public static int MaxSubsetSumNoAdjacentSolution1(int[] array)
+		{
+			if (array.Length == 0)
+			{
+				return 0;
+			}
+			else if (array.Length == 1)
+			{
+				return array[0];
+			}
+			int[] maxSums = (int[])array.Clone();
+			maxSums[1] = Math.Max(array[0], array[1]);
+			for (int i = 2; i < array.Length; i++)
+			{
+				maxSums[i] = Math.Max(maxSums[i - 1], maxSums[i - 2] + array[i]);
+			}
+			return maxSums[array.Length - 1];
+		}
+	}
+}
