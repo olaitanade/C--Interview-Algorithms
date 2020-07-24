e21c32202f39916deb618a117f16e0b9dc99b577
Adetayo Olaitan <olaitan.adetayo.com>
2020-07-24 09:03:46 +0100

    Added Levenshtein Distance algorithm

---
+++ Algorithms/Models/LevenshteinDistance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class LevenshteinDistance
+    {
+		public static int LevenshteinDistanceSolution1(string str1, string str2)
+		{
+			int[,] edits = new int[str2.Length + 1, str1.Length + 1];
+			for (int i = 0; i < str2.Length + 1; i++)
+			{
+				for (int j = 0; j < str1.Length + 1; j++)
+				{
+					edits[i, j] = j;
+				}
+				edits[i, 0] = i;
+			}
+			for (int i = 1; i < str2.Length + 1; i++)
+			{
+				for (int j = 1; j < str1.Length + 1; j++)
+				{
+					if (str2[i - 1] == str1[j - 1])
+					{
+						edits[i, j] = edits[i - 1, j - 1];
+					}
+					else
+					{
+						edits[i, j] = 1 + Math.Min(edits[i - 1, j - 1], Math.Min(edits[i - 1, j], edits[i, j - 1]));
+					}
+				}
+			}
+			return edits[str2.Length, str1.Length];
+		}
+
+	}
+}
