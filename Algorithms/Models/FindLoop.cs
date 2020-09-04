f2023ffb40caa3cddb7bc2150da6548bc7c091a5
Adetayo Olaitan <olaitan.adetayo.com>
2020-09-04 08:33:21 +0100

    added find loop algorithm

---
+++ Algorithms/Models/FindLoop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class FindLoop
+    {
+		public static LinkedList FindLoopSolution(LinkedList head)
+		{
+			LinkedList first = head.next;
+			LinkedList second = head.next.next;
+			while (first != second)
+			{
+				first = first.next;
+				second = second.next.next;
+			}
+			first = head;
+			while (first != second)
+			{
+				first = first.next;
+				second = second.next;
+			}
+			return first;
+		}
+
+		public class LinkedList
+		{
+			public int value;
+			public LinkedList next = null;
+
+			public LinkedList(int value)
+			{
+				this.value = value;
+			}
+		}
+	}
+}
