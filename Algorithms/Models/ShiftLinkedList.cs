5d303e3ca0871bd8919b3b0b92ca4d7c2c47bcc2
Adetayo Olaitan <olaitan.adetayo.com>
2020-09-05 14:38:43 +0100

    added shift linked list algorithm

---
+++ Algorithms/Models/ShiftLinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class ShiftLinkedList
+    {
+		public static LinkedList ShiftLinkedListSolution1(LinkedList head, int k)
+		{
+			int listLength = 1;
+			LinkedList listTail = head;
+			while (listTail.next != null)
+			{
+				listTail = listTail.next;
+				listLength++;
+			}
+
+			int offset = Math.Abs(k) % listLength;
+			if (offset == 0) return head;
+			int newTailPosition = k > 0 ? listLength - offset : offset;
+			LinkedList newTail = head;
+			for (int i = 1; i < newTailPosition; i++)
+			{
+				newTail = newTail.next;
+			}
+
+			LinkedList newHead = newTail.next;
+			newTail.next = null;
+			listTail.next = head;
+			return newHead;
+		}
+
+		public class LinkedList
+		{
+			public int value;
+			public LinkedList next;
+
+			public LinkedList(int value)
+			{
+				this.value = value;
+				next = null;
+			}
+		}
+	}
+}
