593524d3f3c10773b7a2d7a729e6f9f458913c7f
Adetayo Olaitan <olaitan.adetayo.com>
2020-09-05 15:13:38 +0100

    added the interweaving strings algorithm

---
+++ Algorithms/Models/InterweavingStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class InterweavingStrings
+    {
+		public static bool Interweavingstrings(string one, string two, string three)
+		{
+			if (three.Length != one.Length + two.Length)
+			{
+				return false;
+			}
+			return areInterwoven(one, two, three, 0, 0);
+		}
+
+		public static bool areInterwoven(string one, string two, string three, int i, int j)
+		{
+			int k = i + j;
+			if (k == three.Length) return true;
+
+			if (i < one.Length && one[i] == three[k])
+			{
+				if (areInterwoven(one, two, three, i + 1, j)) return true;
+			}
+
+			if (j < two.Length && two[j] == three[k])
+			{
+				return areInterwoven(one, two, three, i, j + 1);
+			}
+
+			return false;
+		}
+
+	}
+}
