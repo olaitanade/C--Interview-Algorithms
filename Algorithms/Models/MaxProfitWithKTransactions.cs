367350b34b699ed8267d1b45c4cd98d362d86631
Adetayo Olaitan <olaitan.adetayo.com>
2020-09-15 15:12:27 +0100

    added max profit with k transactions algorithm

---
+++ Algorithms/Models/MaxProfitWithKTransactions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class MaxProfitWithKTransactions
+    {
+		public static int MaxProfitWithKTransactionsSolution(int[] prices, int k)
+		{
+			if (prices.Length == 0)
+			{
+				return 0;
+			}
+			int[,] profits = new int[k + 1, prices.Length];
+			for (int t = 1; t < k + 1; t++)
+			{
+				int maxThusFar = Int32.MinValue;
+				for (int d = 1; d < prices.Length; d++)
+				{
+					maxThusFar = Math.Max(maxThusFar, profits[t - 1, d - 1] - prices[d - 1]);
+					profits[t, d] = Math.Max(profits[t, d - 1], maxThusFar + prices[d]);
+				}
+			}
+			return profits[k, prices.Length - 1];
+		}
+
+	}
+}
