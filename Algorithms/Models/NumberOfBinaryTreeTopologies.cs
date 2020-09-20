534ce3a8133007e5e62156484322d1b0bb35484a
Adetayo Olaitan <olaitan.adetayo.com>
2020-09-20 23:35:31 +0100

    added number of binary tree topologies

---
+++ Algorithms/Models/NumberOfBinaryTreeTopologies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Algorithms.Models
+{
+    public class NumberOfBinaryTreeTopologies
+    {
+		public static int NumberOfBinaryTreeTopologiesSolution(int n)
+		{
+			if (n == 0)
+			{
+				return 1;
+			}
+			int numberOfTrees = 0;
+			for (int leftTreeSize = 0; leftTreeSize < n; leftTreeSize++)
+			{
+				int rightTreeSize = n - 1 - leftTreeSize;
+				int numberOfLeftTrees = NumberOfBinaryTreeTopologiesSolution(leftTreeSize);
+				int numberOfRightTrees = NumberOfBinaryTreeTopologiesSolution(rightTreeSize);
+				numberOfTrees += numberOfLeftTrees * numberOfRightTrees;
+			}
+			return numberOfTrees;
+		}
+	}
+}
