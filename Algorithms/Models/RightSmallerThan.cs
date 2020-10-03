b9c3852ba209683c2acc4da3c33976c3a5dd77c8
Adetayo Olaitan <olaitan.adetayo.com>
2020-10-03 23:34:22 +0100

    added Right Smaller Than

---
+++ Algorithms/Models/RightSmallerThan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Models
+{
+    public class RightSmallerThan
+    {
+		public static List<int> RightSmallerThanSolution(List<int> array)
+		{
+			List<int> rightSmallerCounts = new List<int>();
+			for (int i = 0; i < array.Count; i++)
+			{
+				int rightSmallerCount = 0;
+				for (int j = i + 1; j < array.Count; j++)
+				{
+					if (array[j] < array[i])
+					{
+						rightSmallerCount++;
+					}
+				}
+				rightSmallerCounts.Add(rightSmallerCount);
+			}
+			return rightSmallerCounts;
+		}
+
+	}
+}
